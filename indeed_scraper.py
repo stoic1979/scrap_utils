97310d67efd291ae812258799d182e88719fc38f
Navjot Singh <weavebytes@gmail.com>
2017-11-01 20:23:59 +0530

    added indeed scraper and homedepot scraper

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -1,34 +1,41 @@
+#
+# Script for scrapping jobs details from Indeed
+#
+
+
 import requests
 from bs4 import BeautifulSoup
+from utils import sleep_scrapper
 
 
 class IndeedScrapper:
 
     def __init__(self, pos, location):
-        post = pos.replace(" ", "+")
-        self.post = post
-        loc = location.replace(" ", "+")
-        self.location = loc
+
+        self.post = pos.replace(" ", "+")
+        self.location = location.replace(" ", "+")
 
     def run(self):
+
         base_url = 'https://www.indeed.co.in/jobs?q=' \
               '%s&l=%s&start=' % (self.post, self.location)
         for j in range(0, 1000, 10):
+            url = ''
             try:
                 url = base_url + str(j)
                 print ''
-                print '[url] ::', url
+                print '[IndeedScrapper] :: fetching data from url:', url
                 r = requests.get(url)
 
                 if not r.status_code == 200:
-                    print "Failed to get content of url: %s" % url
+                    print "[IndeedScrapper] :: Failed to " \
+                          "get content of url: %s" % url
                     return
+
                 html_doc = r.content
 
                 soup = BeautifulSoup(html_doc, 'html.parser')
 
-                # parsing html content  to fet information about python developer
-                # for div in soup.find_all('div', class_='brdr'):
                 for div in soup.find_all('div'):
                     # ignore divs with classes
                     if not div.attrs.has_key('class'):
@@ -37,32 +44,39 @@ class IndeedScrapper:
                     cls = div.attrs['class']
                     if 'row' in cls and 'result' in cls:
                         self.scrap_result_row(div)
+
                         # break
+                sleep_scrapper('IndeedScraper')
             except Exception as exp:
-                print 'run() :: Got exception : %s' % exp
+                print '[HomeDepotScraper] :: run() :: Got exception : ' \
+                      '%s and fetching data from url: %s' % (exp, url)
 
     def scrap_result_row(self, div):
 
-        # title
-        title = div.find('span', class_='company').text.strip()
-        print "title: %s" % title
+        try:
+            # title
+            title = div.find('span', class_='company').text.strip()
+            print "[IndeedScrapper] :: title: %s" % title
 
-        # location
-        span = div.find('span', class_='location')
-        location = span.text.strip()
-        print "location: %s" % location
+            # location
+            span = div.find('span', class_='location')
+            location = span.text.strip()
+            print "[IndeedScrapper] :: location: %s" % location
 
-        # salary
-        sal = ''
-        span = div.find('span', class_='no-wrap')
-        if span:
-            sal = span.text.strip()
-            print "salary: %s" % sal
+            # salary
+            sal = ''
+            span = div.find('span', class_='no-wrap')
+            if span:
+                sal = span.text.strip()
+                print "[IndeedScrapper] :: salary: %s" % sal
 
-        # summary
-        span = div.find('span', class_='summary')
-        summary = span.text.strip()
-        print "summery: %s" % summary
+            # summary
+            span = div.find('span', class_='summary')
+            summary = span.text.strip()
+            print "[IndeedScrapper] :: summery: %s" % summary
+        except Exception as exp:
+            print '[IndeedScrapper] :: scrap_result_row() :: ' \
+                  'Got exception : %s' % exp
 
 if __name__ == '__main__':
     scraper = IndeedScrapper('java', 'mohali punjab')
